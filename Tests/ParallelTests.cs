17c14fc77223aa243818b6882d53e6f6a3e17d08
Dmitry Tarasov <dmitry.tarasov@live.com>
2020-08-03 13:43:29 +0500

    feat: Tests for Parallel

---
+++ Tests/ParallelTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    /// <summary>
+    /// This tests demonstrate how to use Parallel class.
+    /// Parallel provides support for parallel loops and regions.
+    /// </summary> 
+    [TestClass]
+    public class ParallelTests
+    {
+        static void Print(int value)
+        {
+            Debug.Write(value + " ");
+        }
+
+        /// <summary>
+        /// This example demonstrates several approaches to implementing a parallel for loop using multiple language constructs.
+        /// </summary>
+        [DataTestMethod]
+        [DataRow(100)]
+        public void TestParallelFor(int range)
+        {
+            Debug.WriteLine("Using a static method:");
+            Parallel.For(0, range, Print);
+
+            Debug.WriteLine("\nUsing an anonymous delegate:");
+            Parallel.For(0, range, delegate (int ix) { Debug.Write(ix + " "); });
+
+            Debug.WriteLine("\nUsing a lambda expression:");
+            Parallel.For(0, range, (ix) => { Debug.Write(ix + " "); });
+        }
+    }
+}
